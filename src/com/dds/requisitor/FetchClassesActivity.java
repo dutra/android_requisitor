70e4f324fb4e290547179bcd49814c8e961bceeb
Isaque Dutra <isaquefd@gmail.com>
2013-01-12 00:58:59 -0500

    downloading html and outputing to a string

--- src/com/dds/requisitor/FetchClassesActivity.java
+++ src/com/dds/requisitor/FetchClassesActivity.java
@@ -1,5 +1,13 @@
 package com.dds.requisitor;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
@@ -10,7 +18,51 @@ import android.view.Menu;
 public class FetchClassesActivity extends Activity {
 
 	String LOG_TAG = "FetchClassesActivity";
+	Integer GET_LENGTH = 512;
 
+					
+	// Given a URL, establishes an HttpUrlConnection and retrieves
+	// the web page content as a InputStream, which it returns as
+	// a string.
+	private String downloadUrl(String myurl) throws IOException {
+	    InputStream is = null;
+	    // Only display the first GET_LENGTH characters of the retrieved
+	    // web page content.
+	    	        
+	    try {
+	        URL url = new URL(myurl);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setReadTimeout(10000 /* milliseconds */);
+	        conn.setConnectTimeout(15000 /* milliseconds */);
+	        conn.setRequestMethod("GET");
+	        conn.setDoInput(true);
+	        // Starts the query
+	        conn.connect();
+	        int response = conn.getResponseCode();
+	        Log.d(LOG_TAG, "The response is: " + response);
+	        is = conn.getInputStream();
+
+	        // Convert the InputStream into a string
+	        String contentAsString = readIt(is, GET_LENGTH);
+	        return contentAsString;
+	        
+	    // Makes sure that the InputStream is closed after the app is
+	    // finished using it.
+	    } finally {
+	        if (is != null) {
+	            is.close();
+	        } 
+	    }
+	}
+	// Reads an InputStream and converts it to a String.
+	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+	    Reader reader = null;
+	    reader = new InputStreamReader(stream, "UTF-8");        
+	    char[] buffer = new char[len];
+	    reader.read(buffer);
+	    return new String(buffer);
+	}
+	
 	private final class ProgressBarAsyncTask extends AsyncTask<Void, Integer, Boolean> { //<Params, Progress, Result>
 		private ProgressDialog pd;
 		private final String MESSAGE[] = { //Available Messages
@@ -18,6 +70,7 @@ public class FetchClassesActivity extends Activity {
 				"Connecting to mit.edu...",
 				"Done!"
 		};
+		private String http;
 
 		/**
 		 * This method will be called before the execution of the task. Here we 
@@ -77,6 +130,14 @@ public class FetchClassesActivity extends Activity {
 				Thread.sleep(1000);
 				publishProgress(1);
 				Thread.sleep(1000);
+				try {
+					http = new String(downloadUrl("http://student.mit.edu/catalog/m8a.html"));
+					Log.d("HTTP", http);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					Log.d("HTTP","http problem "+e);
+					e.printStackTrace();
+				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				return false;
@@ -103,5 +164,7 @@ public class FetchClassesActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_fetch_classes, menu);
 		return true;
 	}
+	
+	
 
 }
