f6de4bf3b135986a05b4c70d2117cb0065466785
Isaque Dutra <isaquefd@gmail.com>
2013-01-11 22:22:52 -0500

    FetchClassesActivity displaying a timed ProgressBar succesfully

--- src/com/dds/requisitor/MainActivity.java
+++ src/com/dds/requisitor/MainActivity.java
@@ -1,8 +1,10 @@
 package com.dds.requisitor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	public void onClickFetch(View v) {
+		Intent i = new Intent(v.getContext(), FetchClassesActivity.class);
+		startActivity(i);
 	}
 
 }
