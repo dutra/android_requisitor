8573271603a334a43db87337fc2a16e1f81688ac
Isaque Dutra <isaquefd@gmail.com>
2013-01-19 21:16:03 -0500

    Databasehandler+Class+ClassesParser skeleton semi-functional

--- src/com/dds/requisitor/FetchClassesActivity.java
+++ src/com/dds/requisitor/FetchClassesActivity.java
@@ -1,12 +1,21 @@
 package com.dds.requisitor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -18,42 +27,75 @@ import android.view.Menu;
 public class FetchClassesActivity extends Activity {
 
 	String LOG_TAG = "FetchClassesActivity";
-	Integer GET_LENGTH = 512;
-
-					
-	// Given a URL, establishes an HttpUrlConnection and retrieves
-	// the web page content as a InputStream, which it returns as
-	// a string.
-	private String downloadUrl(String myurl) throws IOException {
-	    InputStream is = null;
-	    // Only display the first GET_LENGTH characters of the retrieved
-	    // web page content.
-	    	        
+	Integer GET_LENGTH = 128000;
+	
+	private String downloadJSON(String s){
+	    URL url;
+	    StringBuffer jsonstring = null;
+	    HttpURLConnection connection;       
 	    try {
-	        URL url = new URL(myurl);
-	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	        conn.setReadTimeout(10000 /* milliseconds */);
-	        conn.setConnectTimeout(15000 /* milliseconds */);
-	        conn.setRequestMethod("GET");
-	        conn.setDoInput(true);
-	        // Starts the query
-	        conn.connect();
-	        int response = conn.getResponseCode();
-	        Log.d(LOG_TAG, "The response is: " + response);
-	        is = conn.getInputStream();
+	        url = new URL(s);
 
-	        // Convert the InputStream into a string
-	        String contentAsString = readIt(is, GET_LENGTH);
-	        return contentAsString;
+	    Log.i("System out", "url:" + url);
+	    connection = (HttpURLConnection) url.openConnection();
+	    connection.setConnectTimeout(1000 * 5); // Timeout is in seconds
+	    InputStreamReader is = new InputStreamReader(connection
+	            .getInputStream());
+	    BufferedReader buff = new BufferedReader(is);
+	    jsonstring = new StringBuffer();
+	    String line = "";
+	    do {
+	        line = buff.readLine();
+	        if (line != null)
+	            jsonstring.append(line);
+	    } while (line != null);
+	    } catch (MalformedURLException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	    	e.printStackTrace();
+	    	return null;
 	        
-	    // Makes sure that the InputStream is closed after the app is
-	    // finished using it.
-	    } finally {
-	        if (is != null) {
-	            is.close();
-	        } 
 	    }
+	    return jsonstring.toString().trim();
 	}
+
+		
+	public static String getHttpResponse(URI uri) throws IOException {
+		//Log.d(APP_TAG, "Going to make a get request");
+		StringBuilder response = new StringBuilder();
+		InputStream is = null;
+		try {
+			HttpGet get = new HttpGet();
+			get.setURI(uri);
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpResponse httpResponse = httpClient.execute(get);
+			if (httpResponse.getStatusLine().getStatusCode() == 200) {
+				
+
+				HttpEntity messageEntity = httpResponse.getEntity();
+				is = messageEntity.getContent();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				String line;
+				while ((line = br.readLine()) != null) {
+					response.append(line);
+				}
+				Log.d("demo", "HTTP Get succeeded");
+
+				
+			}
+		} catch (Exception e) {
+			Log.e("demo", e.getMessage());
+		} finally {
+			if (is!=null)
+				is.close();
+		}
+		
+		Log.d("demo", "Done with HTTP getting");
+		return response.toString();
+	}
+	
 	// Reads an InputStream and converts it to a String.
 	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
 	    Reader reader = null;
@@ -68,6 +110,9 @@ public class FetchClassesActivity extends Activity {
 		private final String MESSAGE[] = { //Available Messages
 				"Initializing connection...",
 				"Connecting to mit.edu...",
+				"Downloading m8a",
+				"Parsing",
+				"Error download",
 				"Done!"
 		};
 		private String http;
@@ -126,22 +171,34 @@ public class FetchClassesActivity extends Activity {
 		 */
 		@Override
 		protected Boolean doInBackground(Void... params) {
+			ClassesParser cp = new ClassesParser();
 			try {
-				Thread.sleep(1000);
-				publishProgress(1);
-				Thread.sleep(1000);
-				try {
-					http = new String(downloadUrl("http://student.mit.edu/catalog/m8a.html"));
-					Log.d("HTTP", http);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					Log.d("HTTP","http problem "+e);
-					e.printStackTrace();
+				URI uri = new URI("http://coursews.mit.edu/coursews/?courses=8");
+				publishProgress(2);
+				Thread.sleep(200);
+				
+				//http = new String(downloadUrl("http://student.mit.edu/catalog/m8a.html"));
+				//http = getHttpResponse(uri);
+				if((http = downloadJSON("http://coursews.mit.edu/coursews/?term=2013SP&courses=8"))== null) {
+					publishProgress(4);
+					Thread.sleep(200);
+					return false;
 				}
+				//Log.d("HTTP", http);
+				publishProgress(3);
+				cp.parseClasses(http);
+				
+				Thread.sleep(200);
+				publishProgress(5);
+				Thread.sleep(200);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				return false;
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
+			
 			return true;
 		}
 
