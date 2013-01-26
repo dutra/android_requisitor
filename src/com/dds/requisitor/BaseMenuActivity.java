bd9b15ce6996a6c869865248315767ed738946c3
shoshievass <shoshievass@gmail.com>
2013-01-26 17:27:21 -0500

    trying to fix menu commit

---
+++ src/com/dds/requisitor/BaseMenuActivity.java
@@ -0,0 +1,37 @@
+package com.dds.requisitor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BaseMenuActivity extends Activity{
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_settings:
+        startActivity(new Intent(this, UserPreferencesActivity.class));
+        return true;
+        case R.id.menu_refresh:
+        startActivity(new Intent(this, FetchClassesActivity.class));
+        return true;
+        case R.id.menu_search:
+        startActivity(new Intent(this, SearchClassActivity.class));
+        return true;
+        case R.id.menu_list:
+        startActivity(new Intent(this, MainActivity.class));
+        	
+        return true;
+        default:
+        return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
