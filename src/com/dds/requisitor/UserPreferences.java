33edfa9f5375041bfc3875fcb9d0db2b8a374cc8
Isaque Dutra <isaquefd@gmail.com>
2013-01-25 21:49:57 -0500

    userpreferences

---
+++ src/com/dds/requisitor/UserPreferences.java
@@ -0,0 +1,156 @@
+package com.dds.requisitor;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+
+public class UserPreferences {
+	private static ArrayList<String> termsL;
+	private static ArrayList<String> termsS;
+	private static String name;
+	private static int graduation;
+	private static String grade;
+	private static String courseN;
+	private static String courseS;
+	private static ArrayList<String> courseNall;
+	private static ArrayList<String> courseSall;
+	private static ArrayList<String> courseURLall;
+	private static String currentTerm;
+	private static Context context;
+
+	UserPreferences(Context c) {
+		
+		currentTerm = "Spring 2012";
+		courseNall = new ArrayList<String>();
+		courseSall = new ArrayList<String>();
+		courseURLall = new ArrayList<String>();
+		courseNall.add("8");
+		courseNall.add("6");
+		courseSall.add("Physics");
+		courseSall.add("Electrical Engineering and Computer Science");
+		context = c;
+		
+	}
+
+	private void initializeTerms() {
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		if(grade.equals("Freshman")) {
+			termsS = initializeTermsS(year);
+			termsL = initializeTermsL(year);
+		}
+		if(grade.equals("Sophomore")) {
+			termsS = initializeTermsS(year-1);
+			termsL = initializeTermsL(year-1);
+		}
+		if(grade.equals("Junior")) {
+			termsS = initializeTermsS(year-2);
+			termsL = initializeTermsL(year-2);
+		}
+		if(grade.equals("Senior")) {
+			termsS = initializeTermsS(year-3);
+			termsL = initializeTermsL(year-3);
+		}
+	}
+	private ArrayList<String> initializeTermsS(int initialyear) {
+		ArrayList<String> termsS = new ArrayList<String>();
+		for(int i=0; i<4; i++) {
+			termsS.add(Integer.toString(initialyear+i)+"FA");
+			termsS.add(Integer.toString(initialyear+i)+"SP");
+		}
+		return termsS;
+	}
+	private ArrayList<String> initializeTermsL(int initialyear) {
+		ArrayList<String> termsL = new ArrayList<String>();
+		for(int i=0; i<4; i++) {
+			termsL.add("Fall"+Integer.toString(initialyear+i));
+			termsL.add("Spring"+Integer.toString(initialyear+i));
+		}
+		return termsL;
+	}
+
+	public void setGraduation(int g) {
+		graduation = g;
+	}
+	public int getGraduation() {
+		return graduation;
+	}
+	public void setGrade(String g) {
+		grade = g;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setCourseN(String c) {
+		courseN = c;
+	}
+	public String getCourseN() {
+		return courseN;
+	}
+	public ArrayList<String> getcourseNall() {
+		return courseNall;
+	}
+	public ArrayList<String> getcourseSall() {
+		return courseSall;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String s) {
+		name = s;
+	}
+	public void save() {
+		SharedPreferences userDetails = context.getSharedPreferences("userdetails", context.MODE_PRIVATE);
+		
+		Editor edit = userDetails.edit();
+		edit.clear();
+		edit.putString("COURSEN", courseN);
+		edit.putString("COURSES", courseS);
+		edit.putString("GRADE", grade);
+		
+		
+		edit.putString("COURSENALL", SerializeArray(courseNall));
+		
+		edit.putString("COURSESALL", SerializeArray(courseSall));
+		
+		edit.commit();
+	}
+	public int load() {
+		
+		SharedPreferences userDetails = context.getSharedPreferences("userdetails", context.MODE_PRIVATE);
+				
+		courseN = userDetails.getString("COURSEN", null);
+		if(courseN==null) return 1;
+		courseS = userDetails.getString("COURSES", null);
+		grade = userDetails.getString("GRADE", null);
+		
+		courseNall = ParseArray(userDetails.getString("COURSENALL", null));
+		courseSall = ParseArray(userDetails.getString("COURSESALL", null));
+					
+	return 0;	
+	}
+	
+	public static String SerializeArray(ArrayList<String> array) {
+		String strArr = "";
+        for (int i=0; i<array.size(); i++) {
+            strArr += array.get(i) + ",";
+        }
+        strArr = strArr.substring(0, strArr.length() -1); // get rid of last comma
+        return strArr;
+    }
+
+    public static ArrayList<String> ParseArray(String str) {
+
+        String[] strArr = str.split(",");
+        ArrayList<String> array = new ArrayList<String>();
+        for (int i=0; i<strArr.length; i++) {
+            array.add(strArr[i]);
+        }
+        return array; 
+    }
+
+}
---
+++ src/com/dds/requisitor/UserPreferencesActivity.java
@@ -0,0 +1,69 @@
+package com.dds.requisitor;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class UserPreferencesActivity extends Activity {
+	UserPreferences up;
+	Spinner spCourse;
+	Spinner spYear;
+	EditText etName;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		ArrayList<String> years = new ArrayList<String>();
+		years.add("Freshman");
+		years.add("Sophomore");
+		years.add("Junior");
+		years.add("Senior");
+		up = new UserPreferences(UserPreferencesActivity.this);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_preferences);
+		int load=up.load();
+		
+		spCourse = (Spinner) findViewById(R.id.spCourse);
+		ArrayAdapter<String> dataAdapterCourse = new ArrayAdapter<String>(this,
+			android.R.layout.simple_spinner_item, up.getcourseSall());
+		dataAdapterCourse.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spCourse.setAdapter(dataAdapterCourse);
+		
+		spYear = (Spinner) findViewById(R.id.spYear);
+		ArrayAdapter<String> dataAdapterYear = new ArrayAdapter<String>(this,
+			android.R.layout.simple_spinner_item, years);
+		dataAdapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		spYear.setAdapter(dataAdapterYear);
+		
+		etName = (EditText) findViewById(R.id.etName);
+		if(load==1) {
+			etName.setText(up.getName());
+			spCourse.setSelection(up.getcourseNall().indexOf(up.getCourseN()));
+			spCourse.setSelection(years.indexOf(up.getGrade()));
+		}
+		
+		
+		
+		
+	}
+	public void OnClickSave(Context c) {
+		up.setName(etName.getText().toString());
+		up.setCourseN(up.getcourseNall().get(spCourse.getSelectedItemPosition()));
+		up.setGrade(spYear.getSelectedItem().toString());
+		up.save();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_user_preferences, menu);
+		return true;
+	}
+
+}
