cf63fecf693f0ee1e2586f6500c5543d4a176322
shoshievass <shoshievass@gmail.com>
2013-01-28 01:22:46 -0500

    making sure i didn't forget stuff

---
+++ src/com/dds/requisitor/ImageAdapter.java
@@ -0,0 +1,64 @@
+package com.dds.requisitor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+	private Context mContext;
+
+	public ImageAdapter(Context c) {
+		mContext = c;
+	}
+
+	public int getCount() {
+		return mThumbIds.length;
+	}
+
+	public String getItem(int position) {
+		switch (position) {
+		case 0:
+			return "explore";
+		case 1:
+			return "search";
+		case 2:
+			return "list";
+		case 3:
+			return "up";
+		}
+		return null;
+	}
+
+	public long getItemId(int position) {
+		return 0;
+
+	}
+
+	// create a new ImageView for each item referenced by the Adapter
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageView;
+		if (convertView == null) { // if it's not recycled, initialize some
+									// attributes
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(8, 8, 8, 8);
+		} else {
+			imageView = (ImageView) convertView;
+		}
+
+		imageView.setImageResource(mThumbIds[position]);
+		return imageView;
+	}
+
+	// references to our images
+	private Integer[] mThumbIds = { R.drawable.sample_4, R.drawable.sample_4,
+			R.drawable.sample_4, R.drawable.sample_4,
+
+	};
+
+}
