969167d85630be081e6016cb6ce1cd1cbb1ac95b
Isaque Dutra <isaquefd@gmail.com>
2013-01-28 03:00:44 -0500

    fixing

--- src/com/dds/requisitor/UserPreferencesActivity.java
+++ src/com/dds/requisitor/UserPreferencesActivity.java
@@ -2,11 +2,8 @@ package com.dds.requisitor;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
