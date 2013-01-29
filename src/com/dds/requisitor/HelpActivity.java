6ad304377bf3437f6c5438c2e960ee951be2f031
Isaque Dutra <isaquefd@gmail.com>
2013-01-29 02:35:37 -0500

    helper

--- src/com/dds/requisitor/HelpActivity.java
+++ src/com/dds/requisitor/HelpActivity.java
@@ -12,6 +12,30 @@ public class HelpActivity extends BaseMenuActivity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
 
+		ExpandablePanel panel1 = (ExpandablePanel) findViewById(R.id.ep1);
+		panel1.setOnExpandListener(new ExpandablePanel.OnExpandListener() {
+		    public void onCollapse(View handle, View content) {
+		        Button btn = (Button)handle;
+		        btn.setText("More");
+		    }
+		    public void onExpand(View handle, View content) {
+		        Button btn = (Button)handle;
+		        btn.setText("Less");
+		    }
+		});
+	
+		ExpandablePanel panel2 = (ExpandablePanel) findViewById(R.id.ep2);
+		panel2.setOnExpandListener(new ExpandablePanel.OnExpandListener() {
+		    public void onCollapse(View handle, View content) {
+		        Button btn = (Button)handle;
+		        btn.setText("More");
+		    }
+		    public void onExpand(View handle, View content) {
+		        Button btn = (Button)handle;
+		        btn.setText("Less");
+		    }
+		});
+		
 	}
 	
 //	ExpandablePanel panel = (ExpandablePanel)view.findViewById(R.id.foo);
